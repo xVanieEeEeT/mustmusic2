8ce135bf6e5c61bb895d9cee57a5e5374650b7b3
xVanieEeEeT <42171095+xVanieEeEeT@users.noreply.github.com>
2018-10-25 16:15:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -24,6 +24,27 @@ const prefix = "2";
 /////////////////////////
 ////////////////////////
 
+client.on("message", async message => {
+  if(message.content.startsWith(prefix + "join")) {
+      if(message.author.id === "410778583682777098" || message.author.id == "447727056617340928") {
+      
+    let args = message.content.split(" ").slice(1).join(" ");
+      if(!args) {
+        return message.channel.send("**يرجــى تحديـد روم صوتــي مـع الآمـر ء .**");
+      }
+        let room = message.guild.channels.find(a => a.name === args);
+          room.join() .then(() => {
+            message.channel.send(`**تـم تثبيــت البـوت فـي روم , \`${args}\`**`) .then((m) => {
+              m.delete(5000);
+            });
+          });
+            
+  } else {
+    return message.channel.send("**آنــت لست مؤهــل لآستخـدآم هذا الآمــر ء .**");
+  }
+}
+});
+
 
 client.on('message', message => {
 	var args = message.content.split(' ').slice(1);
